44c739244e5460b65a83409a2f4c70f5a64328a2
Sheba Mathew <sheba.m.mathew@gmail.com>
2019-01-11 19:44:51 -0500

    working on extracting recipe name and ingredients

--- script.js
+++ script.js
@@ -12,99 +12,12 @@ function searchNutrient() {
 function searchFood(){
     fetch(`https://api.edamam.com/search?q=turkey&app_id=4d646771&app_key=3eb644f27fc05523aa0687b2a0361dd9`)
         .then(response => response.json())
-        .then(jsonData => console.log(jsonData))
+        // .then(jsonData => console.log(jsonData.hits[3].recipe))
+        .then(jsonData => console.log(jsonData.hits[3].recipe.label))
         .catch(error => console.error(error));
         // .then(responseJson => console.log(responseJson.report.foods[0].name))
         // .then(responseJson => console.log(responseJson.report.foods[0].nutrients[0].gm))
 }
-
-// const nutrientSearchResult = {
-//     "report": {
-//       "sr": "1",
-//       "groups": "All groups",
-//       "subset": "All foods",
-//       "end": 5,
-//       "start": 0,
-//       "total": 7524,
-//       "foods": [
-//         {
-//           "ndbno": "05711",
-//           "name": "Turkey, retail parts, breast, meat only, cooked, roasted",
-//           "weight": 863,
-//           "measure": "1.0 breast",
-//           "nutrients": [
-//             {
-//               "nutrient_id": "203",
-//               "nutrient": "Protein",
-//               "unit": "g",
-//               "value": "254.67",
-//               "gm": 29.51
-//             }
-//           ]
-//         },
-//         {
-//           "ndbno": "36629",
-//           "name": "Restaurant, Chinese, orange chicken",
-//           "weight": 648,
-//           "measure": "1.0 order",
-//           "nutrients": [
-//             {
-//               "nutrient_id": "203",
-//               "nutrient": "Protein",
-//               "unit": "g",
-//               "value": "93.70",
-//               "gm": 14.46
-//             }
-//           ]
-//         },
-//         {
-//           "ndbno": "01258",
-//           "name": "Egg, white, dried, stabilized, glucose reduced",
-//           "weight": 107,
-//           "measure": "1.0 cup, sifted",
-//           "nutrients": [
-//             {
-//               "nutrient_id": "203",
-//               "nutrient": "Protein",
-//               "unit": "g",
-//               "value": "89.97",
-//               "gm": 84.08
-//             }
-//           ]
-//         },
-//         {
-//           "ndbno": "01136",
-//           "name": "Egg, white, dried, powder, stabilized, glucose reduced",
-//           "weight": 107,
-//           "measure": "1.0 cup, sifted",
-//           "nutrients": [
-//             {
-//               "nutrient_id": "203",
-//               "nutrient": "Protein",
-//               "unit": "g",
-//               "value": "88.17",
-//               "gm": 82.4
-//             }
-//           ]
-//         },
-//         {
-//           "ndbno": "36633",
-//           "name": "Restaurant, Chinese, sesame chicken",
-//           "weight": 547,
-//           "measure": "1.0 order",
-//           "nutrients": [
-//             {
-//               "nutrient_id": "203",
-//               "nutrient": "Protein",
-//               "unit": "g",
-//               "value": "78.39",
-//               "gm": 14.33
-//             }
-//           ]
-//         }
-//       ]
-//     }
-//   }
 
 function getNutrientInfo(responseJson) {
     const nutrientInfo = responseJson.report.foods; 
@@ -119,15 +32,21 @@ function getNutrientInfo(responseJson) {
     }
 }
 
+function getRecipe(jsonData) {
+    const recipeName = jsonData.hits[3].recipe.label; 
+    const mainIngredient = jsonData.hits[3].recipe.ingredients[0]; 
+    console.log(`${recipeName} with ${mainIngredient}`)
+}
+
 function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
-      searchNutrient();
+    //   searchNutrient();
       searchFood(); 
+      getRecipe(); 
     });
 }
 
-  
 $(function() {
     console.log('Waiting...');
     watchForm();
